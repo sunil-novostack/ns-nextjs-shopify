7626406af90f670641db72a4635d24a8a9852f5e
sunil kumar mutaka <sunil.kumar@novostack.com>
2021-01-22 19:16:00 +0530

    get products query change

--- components/ProductList.js
+++ components/ProductList.js
@@ -56,45 +56,7 @@ function ProductList (){
     console.log('stored products',data);    
     return(
         <Card>
-          <ResourceList
-                showHeader
-                resourceName={{ singular: 'Product',plural: 'Products' }}
-                items={ data.nodes }
-                renderItem={ item => {
-                    const media = (
-                        <Thumbnail 
-                            source={
-                                item.images.edges[0] ? item.images.edges[0].node.originalSrc :''
-                            }
-                            alt={
-                                item.images.edges[0] ? item.images.edges[0].altText : ''
-                            }
-                        />
-                    );
-                    const price  = item.variants.edges[0].node.price;
-                    const currencyCode = item.priceRangeV2.minVariantPrice.currencyCode;
-                    return(
-                        <ResourceList.Item
-                            id={item.id}
-                            media={media}
-                            accessibilityLabel={`view Details for ${item.title}`}
-                        >
-                        <Stack>
-                            <Stack.Item fill>
-                                <h3>
-                                    <TextStyle variation='strong'>
-                                        {item.title}
-                                    </TextStyle>
-                                </h3>
-                            </Stack.Item>
-                            <Stack.Item>
-                                <p>{currencyCode} {price}</p>
-                            </Stack.Item>
-                        </Stack>
-                        </ResourceList.Item>
-                    )
-                }}
-            />
+          
         </Card>
     )
 }
