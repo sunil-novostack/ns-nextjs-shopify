628800b6c463f62aaaad7848f3f6edd60b6f1746
sunil kumar mutaka <sunil.kumar@novostack.com>
2021-01-22 13:14:13 +0530

    add frame component

--- pages/store-products.js
+++ pages/store-products.js
@@ -1,8 +1,15 @@
 import React,{Component} from 'react';
+import {
+  Page,
+  Frame,
+  Layout,
+  Card,
+  Heading,
+} from '@shopify/polaris';
 import NarvigationBar from '../components/NavigationBar';
 
 
-export default class StoreProducts extends Component{
+export default class Storeproducts extends Component{
   render(){
     return (
       <Frame
