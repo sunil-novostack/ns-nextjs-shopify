1abe05e1c05bac0ecbfaf344043a8afc34b52b64
sunil kumar mutaka <sunil.kumar@novostack.com>
2021-01-23 15:52:42 +0530

    query update

--- components/ProductList.js
+++ components/ProductList.js
@@ -6,9 +6,6 @@ import { Card, ResourceList, Stack, TextStyle, Thumbnail } from '@shopify/polari
 
 const GET_FIRST_PRODUCTS = gql`
 query getProducts($row:Int!){
-  shop{
-    currencyCode
-  }
   products(first:$row) {
     edges {
       cursor
@@ -40,61 +37,51 @@ query getProducts($row:Int!){
 }
 `;
 
-/*
-const GET_FIRST_PRODUCTS = gql`
-query getProducts($row:Int!){
-  products(first: $row) {
-    edges {
-      cursor
-      node{
-        id
-        title
-        variants(first:1){
-          edges{
-            node{
-              price
-            }
-          }
-        }
-        priceRangeV2{
-          minVariantPrice{
-            amount
-            currencyCode
-          }
-        }
-        images(first:1){
-          edges{
-            node{
-              id
-              originalSrc
-              altText
-              width
-              height
-              transformedSrc
-            }
-          }
-        }
-      }
-    }
-    pageInfo {
-      hasNextPage
-      hasPreviousPage
-    }
-  }
-}
-`;
-*/
-
 function ProductList (){
 
     const {loading, error, data} = useQuery(GET_FIRST_PRODUCTS, { variables: { row: 20 } });
     if (loading) return <div>loading...</div>
     if (error) return <div>{error.message}</div>
     
-    console.log('stored products',data);    
+    console.log('stored products',data.products.edges);    
     return(
         <Card>
-          
+          <ResourceList
+            items={data.products.edges}
+            renderItem={ item => {
+              const image = item.node.images.edges[0];
+              const media = (
+                <Thumbnail
+                  source={
+                    image ? image.originalSrc : ''
+                  }
+                />
+              );
+              const price = item.node.variants.edges[0].price
+              return(
+                <ResourceList.Item
+                  id={item.id}
+                  media={media}
+                  accessibilityLabel={`View Details for ${item.title}`}
+                >
+                  <Stack>
+                    <Stack.Item fill>
+                      <h3>
+                          <TextStyle variation='strong'>
+                              {item.title}
+                          </TextStyle>
+                      </h3>
+                    </Stack.Item>
+                    <Stack.Item>
+                        <p>${price}</p>
+                    </Stack.Item>
+                  </Stack>
+                </ResourceList.Item>
+              )
+            }}
+          >
+
+          </ResourceList>
         </Card>
     )
 }
--- server.js
+++ server.js
@@ -39,21 +39,6 @@ app.prepare().then(() => {
       },
     }),
   );
-/*
-  server.use(graphQLProxy({ version: ApiVersion.April20}));
- 
-  server.use('/',verifyRequest(), async (ctx) => {
-    try{
-      await handle(ctx.req, ctx.res);
-      ctx.respond = false;
-      ctx.res.statusCode = 200;
-    }catch(e){
-      console.log(e)
-    }
-    
-  })
-  
-*/
   server.use(graphQLProxy({ version: ApiVersion.April20}));
   server.use(verifyRequest());
   server.use(async (ctx) => {
