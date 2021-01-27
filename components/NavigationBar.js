f8c5c7887da4b095d0fc0da7d55c9f71fb998aa2
sunil kumar mutaka <sunil.kumar@novostack.com>
2021-01-27 15:49:16 +0530

    add

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -24,6 +24,7 @@ const  NarvigationBar = (
                     {
                         label: 'Store Products',
                         icon: FaShoppingCart,
+                        url:'/store-products'
                     },
                     {
                         label: 'Setting',
