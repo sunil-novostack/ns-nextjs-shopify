905ab0fd1124a153891a473bbea08a13e5ca0bdd
sunil kumar mutaka <sunil.kumar@novostack.com>
2021-01-29 11:50:51 +0530

    loading on product conditional

--- pages/import-products.js
+++ pages/import-products.js
@@ -17,6 +17,7 @@ import NarvigationBar from '../components/NavigationBar';
 
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/react-hooks';
+import { FaUserAltSlash } from 'react-icons/fa';
 
 const ADD_NEW_PRODUCT = gql`
     mutation addNewProduct($input: ProductInput!, $media: [CreateMediaInput!]!) {
@@ -31,7 +32,9 @@ const ADD_NEW_PRODUCT = gql`
 export default function Importproducts (){
     const [items,setItems] = useState([])
     const [AddNewProduct] = useMutation(ADD_NEW_PRODUCT);
+    const [loading,setLoading] = useState(false)
     useEffect(() => {
+        loading = true;
         firebase.auth().onAuthStateChanged( async (user)=>{
             if(!user){
                 Router.push('/signin')
@@ -46,8 +49,9 @@ export default function Importproducts (){
                products.push(doc.data())
             })
             setItems(products)
+            loading = false;
         })
-    },[items]) 
+    },[items,loading]) 
     const HandleClickImportProduct = async (product) =>{
 
     }
@@ -58,52 +62,60 @@ export default function Importproducts (){
             <Page title={<Heading>Imported Products</Heading>} fullWidth>
                 <Card sectioned>
                     <div className="product-list-items">
-                        {items.length > 0
-                        ?
-                            items.map( (product,index)=>{
-                                return(
-                                    <div className="product-item" id={"item-"+index} key={index}>
-                                        <div className="image-holder">
-                                            <img src={product.image} />                                         
-                                        </div>
-                                        <div className="item-bottom">
-                                            <h2 className="item-title">{product.title}</h2>
-                                            <h2 className="item-price">US $ {product.price}</h2>
-                                            <Button
-                                                name="importtostore"
-                                                submit="false"
-                                                primary={true}
-                                                size="slim"
-                                                onClick={() => AddNewProduct({
-                                                    variables:{
-                                                        input: {
-                                                            title : product.title,
-                                                            descriptionHtml:product.description,
-                                                            variants: [
-                                                                {
-                                                                    price: product.price
-                                                                }
-                                                            ]
-                                                        },
-                                                        media:[
-                                                            {
-                                                                originalSource:product.image,
-                                                                alt:"Sample image testing",
-                                                                mediaContentType:"IMAGE"
-                                                            }
-                                                        ]
-                                                    }
-                                                })}
-                                            >
-                                            Import To Store
-                                            </Button>
-                                        </div>
-                                    </div>
+                        {loading
+                            ?
+                                (
+                                    <p>Please Wait While product fetching from server...</p>
                                 )
-                            })
                             :
-                            <p>No Items Found...</p>                                   
-                        }                         
+                                (items.length > 0
+                                    ?
+                                    items.map( (product,index)=>{
+                                        return(
+                                            <div className="product-item" id={"item-"+index} key={index}>
+                                                <div className="image-holder">
+                                                    <img src={product.image} />                                         
+                                                </div>
+                                                <div className="item-bottom">
+                                                    <h2 className="item-title">{product.title}</h2>
+                                                    <h2 className="item-price">US $ {product.price}</h2>
+                                                    <Button
+                                                        name="importtostore"
+                                                        submit="false"
+                                                        primary={true}
+                                                        size="slim"
+                                                        onClick={() => AddNewProduct({
+                                                            variables:{
+                                                                input: {
+                                                                    title : product.title,
+                                                                    descriptionHtml:product.description,
+                                                                    variants: [
+                                                                        {
+                                                                            price: product.price
+                                                                        }
+                                                                    ]
+                                                                },
+                                                                media:[
+                                                                    {
+                                                                        originalSource:product.image,
+                                                                        alt:"Sample image testing",
+                                                                        mediaContentType:"IMAGE"
+                                                                    }
+                                                                ]
+                                                            }
+                                                        })}
+                                                    >
+                                                    Import To Store
+                                                    </Button>
+                                                </div>
+                                            </div>
+                                        )
+                                    })
+                                    :
+                                    <p>No Product Listed Yet...</p>
+                                )
+                                
+                        }                     
                     </div>
                 </Card>
             </Page>
