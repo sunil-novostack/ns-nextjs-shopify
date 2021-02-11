857796d5207aed0690488326d6dc975290bc1333
sunil kumar mutaka <sunil.kumar@novostack.com>
2021-02-11 13:29:10 +0530

    design changed

--- pages/import-products.js
+++ pages/import-products.js
@@ -119,12 +119,13 @@ export default function Importproducts (){
                                     </div>
                                 </div>
                                 <div className="description">
+                                    <TextStyle variation="subdued">Description</TextStyle>
                                     <DisplayText size="small">
                                         is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
                                     </DisplayText>
                                 </div>
                                 <div className="tags">
-                                    <Tag>Movie</Tag><Tag>Series</Tag><Tag>Collectibles</Tag><Tag>Add Tag</Tag>
+                                    <Tag>Movie</Tag><Tag>Series</Tag><Tag>Collectibles</Tag>
                                 </div>
                                 </ResourceList.Item>
                             )
