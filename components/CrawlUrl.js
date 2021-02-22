fcae920744beb565586914fea411a935d80567d3
sunil kumar mutaka <sunil.kumar@novostack.com>
2021-02-22 13:41:08 +0530

    add api key

--- components/CrawlUrl.js
+++ components/CrawlUrl.js
@@ -29,6 +29,9 @@ export default class CrawlUrl extends Component{
     handleFecthProductSubmit = async (_event) => {
         
         const response = await axios({
+            header:{
+                'x-api-key':'0c63e893-4bc0-44ca',
+            },
             url : '/detail',
             method:'post',
             baseURL:'https://ecomapp.io/data',
