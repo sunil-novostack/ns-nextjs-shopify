30e8551dcfef5f3012d85ef395c517e0b8025c54
sunil kumar mutaka <sunil.kumar@novostack.com>
2021-02-23 15:46:03 +0530

    add

--- components/CrawlUrl.js
+++ components/CrawlUrl.js
@@ -29,7 +29,7 @@ export default class CrawlUrl extends Component{
     handleFecthProductSubmit = async (_event) => {
         
         const response = await axios({
-            hearder:{
+            hearders:{
                 'x-api-key':'0c63e893-4bc0-44ca',
                 'Access-Control-Allow-Origin': '*',
                 'Content-Type': 'application/json',
