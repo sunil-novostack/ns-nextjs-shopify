93dae447aa711ce578fb0cfc9e5c9e681ffdda53
sunil kumar mutaka <sunil.kumar@novostack.com>
2021-02-23 11:47:28 +0530

    add cors in server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
 const dotenv = require('dotenv');
 const { verifyRequest } = require('@shopify/koa-shopify-auth');
 const session = require('koa-session');
+const Cors = require('cors');
 
 /*
 const KoaRouter = require('koa-router');
@@ -38,7 +39,7 @@ router.get("/api/products",async(ctx)=>{
 
 app.prepare().then(() => {
   const server = new Koa();
-
+  server.use(Cors());
   server.use(session({ sameSite: 'none', secure: true }, server));
   server.keys = [SHOPIFY_API_SECRET_KEY];
   
