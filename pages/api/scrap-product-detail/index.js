a56db50d2c2dafde7890f23d1a64566876273e8b
sunil kumar mutaka <sunil.kumar@novostack.com>
2021-02-25 16:57:31 +0530

    add web scrapping

--- pages/api/scrap-product-detail/index.js
+++ pages/api/scrap-product-detail/index.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
+import cheerio from 'cheerio';
 
 export default async (req,res) => {
     const {method,query} = req;
     switch(method){
         case 'GET':
             try{
+                /*
                 const response = await axios({
                   headers: {
                     "x-api-key": "0c63e893-4bc0-44ca",
@@ -19,10 +21,26 @@ export default async (req,res) => {
                     db_entry: 0,
                   },
                 });
-
-               
-                const products = {}
-                res.status(200).json({success:true,productDetail:response.data})
+                */
+                const {response} = await axios.get(query.url)
+                const $ = cheerio.load(response)
+                let images = [];
+                $('.slider-frame .slider-list li').each((i,li) => {
+                    let img = $(li).find('img').attr('src')
+                    let image = img.match(/.*?[?!.]/g);
+                    images.push(image[0])
+                })
+                const products = {
+                    product_title : $('h1.prod-ProductTitle').text(),
+                    description:$('div.about-product-standard').html(),
+                    product_price:$('.prod-PriceHero span.price-characteristic').text(),
+                    currency:{
+                        currency_type:$('.prod-PriceHero span.price-currency').attr('content'),
+                        currency_sign:$('.prod-PriceHero span.price-currency').text()
+                    },
+                    images:images
+                }
+                res.status(200).json({success:true,productDetail:products})
             }catch(error){
                 res.status(400).json({success:false})
             }
